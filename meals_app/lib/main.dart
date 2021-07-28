b9285adf1a1b8616ae3b8d0a2977571d61f91a7d
MarceloJSCesar <jsdevmarcelo@gmail.com>
2021-07-28 08:37:02 -0700

    model and dummy data const variable created

--- meals_app/lib/main.dart
+++ meals_app/lib/main.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
-import '../app/meals_app.dart';
 
 void main() {
   runApp(MealsApp());
+}
+
+class MealsApp extends StatefulWidget {
+  const MealsApp({Key key}) : super(key: key);
+
+  @override
+  _MealsAppState createState() => _MealsAppState();
+}
+
+class _MealsAppState extends State<MealsApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
 }
