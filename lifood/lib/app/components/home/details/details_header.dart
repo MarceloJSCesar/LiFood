e9f3b63a0e2ff7f0126ee7d439a6287b898a1e7d
MarceloJSCesar <jsdevmarcelo@gmail.com>
2021-08-02 20:08:31 -0400

    debuging ... details view bug fixes

---
+++ lifood/lib/app/components/home/details/details_header.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:typicons_flutter/typicons_flutter.dart';
+
+class DetailsHeader extends StatelessWidget {
+  final recipeName;
+  final double recipeRating;
+  final recipeImageUrl;
+  final recipeTotalTime;
+  const DetailsHeader({
+    Key key,
+    @required this.recipeName,
+    @required this.recipeRating,
+    @required this.recipeImageUrl,
+    @required this.recipeTotalTime,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          height: 200,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: NetworkImage(recipeImageUrl),
+            ),
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(22),
+              bottomRight: Radius.circular(22),
+            ),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              IconButton(
+                color: Colors.black,
+                icon: Icon(Icons.arrow_back),
+                iconSize: 25,
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+              Expanded(
+                child: Container(),
+              ),
+              Container(
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.black.withOpacity(0.3),
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(22),
+                    bottomRight: Radius.circular(22),
+                  ),
+                ),
+                child: Container(
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: 15, vertical: 6),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        recipeName,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(recipeTotalTime),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: <Widget>[
+                              Icon(
+                                Typicons.star,
+                                color: Colors.white,
+                                size: 20,
+                              ),
+                              SizedBox(
+                                width: 3,
+                              ),
+                              Text(recipeRating.toStringAsFixed(1)),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lifood/lib/app/views/home/recipes/recipe_details.dart
+++ lifood/lib/app/views/home/recipes/recipe_details.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lifood/app/components/home/details/details_header.dart';
 import 'package:typicons_flutter/typicons_flutter.dart';
 
 class RecipeDetail extends StatelessWidget {
@@ -17,82 +18,19 @@ class RecipeDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              Container(
-                height: 200,
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    fit: BoxFit.cover,
-                    image: NetworkImage(recipeImageUrl),
-                  ),
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(22),
-                    bottomRight: Radius.circular(22),
-                  ),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    IconButton(
-                      color: Colors.black,
-                      icon: Icon(Icons.arrow_back),
-                      iconSize: 25,
-                      onPressed: () => Navigator.of(context).pop(),
-                    ),
-                    Expanded(
-                      child: Container(),
-                    ),
-                    Container(
-                      height: 50,
-                      decoration: BoxDecoration(
-                        color: Colors.black.withOpacity(0.3),
-                        borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(22),
-                          bottomRight: Radius.circular(22),
-                        ),
-                      ),
-                      child: Container(
-                        margin: const EdgeInsets.symmetric(
-                            horizontal: 15, vertical: 6),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(recipeName),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: <Widget>[
-                                Text(recipeTotalTime),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: <Widget>[
-                                    Icon(
-                                      Typicons.star,
-                                      color: Colors.white,
-                                      size: 20,
-                                    ),
-                                    SizedBox(
-                                      width: 3,
-                                    ),
-                                    Text(recipeRating.toStringAsFixed(1)),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
+            child: Column(
+          children: <Widget>[
+            DetailsHeader(
+              recipeName: recipeName,
+              recipeRating: recipeRating,
+              recipeImageUrl: recipeImageUrl,
+              recipeTotalTime: recipeTotalTime,
+            )
+          ],
+        )),
       ),
     );
   }
